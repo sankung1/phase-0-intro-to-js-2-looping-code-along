561293cdcd2fa65c3481af9c63591a86c4c42387
Sankung Sillah <ssillah306@gmail.com>
2023-03-03 22:48:57 -0500

    completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+
+    const message = [];
+    for(let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+
+    return message;
+}
+
+
+function countDown(num){
+
+    while(num !== -1){
+        console.log(num)
+        num--;
+    }
+}
